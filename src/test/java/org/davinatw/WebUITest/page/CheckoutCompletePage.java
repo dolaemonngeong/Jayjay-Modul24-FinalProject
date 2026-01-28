b97c555b8a273dc8034f4152435f09166b56b648
Davina T W <91788095+dolaemonngeong@users.noreply.github.com>
2026-01-28 17:08:33 +0700

    Fix: add wait in validateCheckoutComplatePage

--- src/test/java/org/davinatw/WebUITest/page/CheckoutCompletePage.java
+++ src/test/java/org/davinatw/WebUITest/page/CheckoutCompletePage.java
@@ -2,6 +2,10 @@ package org.davinatw.WebUITest.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CheckoutCompletePage {
     WebDriver driver;
@@ -14,11 +18,19 @@ public class CheckoutCompletePage {
     }
 
     public boolean validateCheckoutCompletePage(){
-        return driver.getCurrentUrl().contains("/checkout-complete.html");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            // This waits until the URL actually contains /cart.html
+            System.out.println("Waiting for checkoutCompletePage URL... Current is:" + driver.getCurrentUrl());
+            return wait.until(ExpectedConditions.urlContains("/checkout-completee.html"));
+        } catch (Exception e) {
+            // If it times out, print the actual URL to help you debug
+            System.out.println("Failed to reach checkoutCompletePage. Final URL was: " + driver.getCurrentUrl());
+            return false;
+        }
+
     }
 
-    public void clickBackHomeButton(){
-        driver.findElement(backHomeButton).click();
-    }
 
 }
